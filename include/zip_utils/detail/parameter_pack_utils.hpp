59ab6601dd40e49cc2962d78b92d15a8d3aaa215
Eugene Kravchenko <zzzheka97@gmail.com>
2021-08-25 14:25:32 +0300

    replace tabs with spaces

--- include/zip_utils/detail/parameter_pack_utils.hpp
+++ include/zip_utils/detail/parameter_pack_utils.hpp
@@ -5,90 +5,90 @@
 
 namespace zip_utils::detail::parameter_pack_utils {
 
-	template<size_t Size, template<typename> typename Criteria, typename... Ts>
-	struct filter_impl;
+    template<size_t Size, template<typename> typename Criteria, typename... Ts>
+    struct filter_impl;
 
-	template<size_t... Is>
-	struct Indices {
-		static constexpr size_t size = sizeof...(Is);
-	};
+    template<size_t... Is>
+    struct Indices {
+        static constexpr size_t size = sizeof...(Is);
+    };
 
-	template<size_t Size, template<typename> typename Criteria>
-	struct filter_impl<Size, Criteria> {
-		using indices = Indices<>;
-		using type = std::tuple<>;
-	};
+    template<size_t Size, template<typename> typename Criteria>
+    struct filter_impl<Size, Criteria> {
+        using indices = Indices<>;
+        using type = std::tuple<>;
+    };
 
-	template<typename T, typename NextTuple>
-	struct tuple_append {
-		template<typename... Ts>
-		static std::tuple<T, Ts...>* impl(std::tuple<Ts...>*) {
-			return nullptr;
-		}
+    template<typename T, typename NextTuple>
+    struct tuple_append {
+        template<typename... Ts>
+        static std::tuple<T, Ts...>* impl(std::tuple<Ts...>*) {
+            return nullptr;
+        }
 
-		using type = std::decay_t<decltype(*impl(static_cast<NextTuple*>(nullptr)))>;
-	};
+        using type = std::decay_t<decltype(*impl(static_cast<NextTuple*>(nullptr)))>;
+    };
 
-	template<size_t I, typename NextSeq>
-	struct integer_sequence_append {
-		template<size_t... Is>
-		static Indices<I, Is...>* impl(Indices<Is...>*) {
-			return nullptr;
-		}
+    template<size_t I, typename NextSeq>
+    struct integer_sequence_append {
+        template<size_t... Is>
+        static Indices<I, Is...>* impl(Indices<Is...>*) {
+            return nullptr;
+        }
 
-		using indices = std::decay_t<decltype(*impl(static_cast<NextSeq*>(nullptr)))>;
-	};
+        using indices = std::decay_t<decltype(*impl(static_cast<NextSeq*>(nullptr)))>;
+    };
 
-	template<size_t Size,
-			 template<typename> typename Criteria,
-			 typename T, typename... Ts>
-	struct filter_impl<Size, Criteria, T, Ts...> {
-		static constexpr bool is_ok = Criteria<T>::value;
-		using next = filter_impl<Size, Criteria, Ts...>;
-		static constexpr size_t index = Size - sizeof...(Ts) - 1;
+    template<size_t Size,
+             template<typename> typename Criteria,
+             typename T, typename... Ts>
+    struct filter_impl<Size, Criteria, T, Ts...> {
+        static constexpr bool is_ok = Criteria<T>::value;
+        using next = filter_impl<Size, Criteria, Ts...>;
+        static constexpr size_t index = Size - sizeof...(Ts) - 1;
 
-		using indices = std::conditional_t<is_ok,
-										   typename integer_sequence_append<index, typename next::indices>::indices,
-										   typename next::indices>;
-		using type = std::conditional_t<is_ok,
-										typename tuple_append<T, typename next::type>::type,
-										typename next::type>;
-	};
+        using indices = std::conditional_t<is_ok,
+                                           typename integer_sequence_append<index, typename next::indices>::indices,
+                                           typename next::indices>;
+        using type = std::conditional_t<is_ok,
+                                        typename tuple_append<T, typename next::type>::type,
+                                        typename next::type>;
+    };
 
-	template<template<typename> typename Func, typename Tuple>
-	struct map_impl {
-		template<typename... Ts>
-		static std::tuple<typename Func<Ts>::type...>* impl(std::tuple<Ts...>*) {
-			return nullptr;
-		}
+    template<template<typename> typename Func, typename Tuple>
+    struct map_impl {
+        template<typename... Ts>
+        static std::tuple<typename Func<Ts>::type...>* impl(std::tuple<Ts...>*) {
+            return nullptr;
+        }
 
-		using type = std::decay_t<decltype(*impl(static_cast<Tuple*>(nullptr)))>;
-	};
+        using type = std::decay_t<decltype(*impl(static_cast<Tuple*>(nullptr)))>;
+    };
 
-	template<template<typename> typename Criteria, typename... Ts>
-	using filter = typename filter_impl<sizeof...(Ts), Criteria, Ts...>::type;
+    template<template<typename> typename Criteria, typename... Ts>
+    using filter = typename filter_impl<sizeof...(Ts), Criteria, Ts...>::type;
 
-	template<template<typename> typename Criteria, typename... Ts>
-	using filter_indices = typename filter_impl<sizeof...(Ts), Criteria, Ts...>::indices;
+    template<template<typename> typename Criteria, typename... Ts>
+    using filter_indices = typename filter_impl<sizeof...(Ts), Criteria, Ts...>::indices;
 
-	template<template<typename> typename Func, typename Tuple>
-	using map = typename map_impl<Func, Tuple>::type;
+    template<template<typename> typename Func, typename Tuple>
+    using map = typename map_impl<Func, Tuple>::type;
 
-	template<template<typename> typename Criteria, typename... Args>
-	bool constexpr at_least_one_is = (Criteria<Args>::value || ... || false);
+    template<template<typename> typename Criteria, typename... Args>
+    bool constexpr at_least_one_is = (Criteria<Args>::value || ... || false);
 
-	template<size_t Idx, size_t... Indices>
-	constexpr size_t count_less_than = ((Indices < Idx ? 1 : 0) + ... + 0);
+    template<size_t Idx, size_t... Indices>
+    constexpr size_t count_less_than = ((Indices < Idx ? 1 : 0) + ... + 0);
 
-	template<size_t Idx, size_t... Indices>
-	constexpr size_t is_in = ((Idx == Indices) || ... || false);
+    template<size_t Idx, size_t... Indices>
+    constexpr size_t is_in = ((Idx == Indices) || ... || false);
 
-	template<template<typename> typename Constructor>
-	struct wrap {
-		template<typename T>
-		struct impl {
-			using type = Constructor<T>;
-		};
-	};
+    template<template<typename> typename Constructor>
+    struct wrap {
+        template<typename T>
+        struct impl {
+            using type = Constructor<T>;
+        };
+    };
 
 }// namespace zip_utils::detail::parameter_pack_utils
--- include/zip_utils/zip_utils.hpp
+++ include/zip_utils/zip_utils.hpp
@@ -6,26 +6,26 @@
 
 namespace zip_utils {
 
-	template<std::ranges::forward_range... Containers>
-	constexpr auto zip(Containers&&... containers)
-	noexcept((detail::utils::is_nothrow_beg_end_copy_constructible_v<Containers> && ...)) {
-		using namespace detail::impl;
-		using namespace detail::parameter_pack_utils;
+    template<std::ranges::forward_range... Containers>
+    constexpr auto zip(Containers&&... containers)
+    noexcept((detail::utils::is_nothrow_beg_end_copy_constructible_v<Containers> && ...)) {
+        using namespace detail::impl;
+        using namespace detail::parameter_pack_utils;
 
-		if constexpr (at_least_one_is<std::is_rvalue_reference, Containers&&...>) {
-			return make_zip_impl_with_rvalue_collections(std::forward<Containers>(containers)...);
-		}
-		else {
-			return make_zip_impl(std::forward<Containers>(containers)...);
-		}
-	}
+        if constexpr (at_least_one_is<std::is_rvalue_reference, Containers&&...>) {
+            return make_zip_impl_with_rvalue_collections(std::forward<Containers>(containers)...);
+        }
+        else {
+            return make_zip_impl(std::forward<Containers>(containers)...);
+        }
+    }
 
-	template<std::ranges::forward_range... Containers>
-	constexpr auto enumerate(Containers&&... containers)
-	noexcept(noexcept(zip(std::forward<Containers>(containers)...))) {
-		detail::counter::counter counter;
-		return zip(counter,
-				   std::forward<Containers>(containers)...);
-	}
+    template<std::ranges::forward_range... Containers>
+    constexpr auto enumerate(Containers&&... containers)
+    noexcept(noexcept(zip(std::forward<Containers>(containers)...))) {
+        detail::counter::counter counter;
+        return zip(counter,
+                   std::forward<Containers>(containers)...);
+    }
 
 }// namespace zip_utils
